993efffa85df966d002c2f93f5323df0e8eff9e0
Vaishnavi <65657414+vaishnaviayyagari@users.noreply.github.com>
2021-03-01 23:03:25 +0530

    have successfully implemented the method
    findRestaurantByName()

---
+++ RestaurantService.java
@@ -0,0 +1,45 @@
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestaurantService {
+    private static List<Restaurant> restaurants = new ArrayList<>();
+
+
+
+
+    public  RestaurantService()
+    {
+        restaurants.add(new Restaurant("Amelias Cafe","Chennai","10:00:00","09:00:00"));
+    }
+
+    public Restaurant findRestaurantByName(String restaurantName)
+    {
+    for (Restaurant restaurant:restaurants)
+    {
+        if(restaurant.getName().equals(restaurantName));
+        {
+            return  restaurant;
+        }
+    }
+    return  null;
+    }
+
+
+    public Restaurant addRestaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        Restaurant newRestaurant = new Restaurant(name, location, openingTime, closingTime);
+        restaurants.add(newRestaurant);
+        return newRestaurant;
+    }
+
+    public Restaurant removeRestaurant(String restaurantName) throws restaurantNotFoundException {
+        Restaurant restaurantToBeRemoved = findRestaurantByName(restaurantName);
+        restaurants.remove(restaurantToBeRemoved);
+        return restaurantToBeRemoved;
+    }
+
+    public List<Restaurant> getRestaurants()
+    {
+        return restaurants;
+    }
+}
